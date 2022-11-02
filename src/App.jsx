6482b4ee9667887fc21cff9c93fffd993fadc6d1
Antonio Paulo Dambujo <pdambujo@gmail.com>
2022-11-02 20:40:36 +0200

    initialize react project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import { Header } from './components';
+
+function App() {
+  return (
+    <div className="conatiner">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
